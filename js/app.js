f320375e1fc892772cbb62a30d8ae5cc38c69473
Lefort <jordan.lefort@laposte.net>
2017-11-16 13:29:09 +0100

    ajout correction

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
-console.log('hello world');
-
 function addition(a, b){
-    return a+b;
+    return a + b;
 }
 
-console.log(addition(15,25));
+console.log(addition(5, 5));
+
+function substract(a, b){
+    return a - b;
+}
+
+console.log(substract(10, 5));
+
+function multiply(a, b){
+    return a * b;
+}
+
+console.log(multiply(10, 10));
+
+function divice(a, b){
+    if(b !== 0){
+        return a/b;
+    } 
+    return "Ou pas";
+}
+
+console.log(divice(10, 0));
+console.log(divice(10, 5));
